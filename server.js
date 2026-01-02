3ae81edaf4401c514598026ba136459fe3a5750c
AkshatRathod-dev <akshatrathodtempmail@gmail.com>
2026-01-03 00:09:11 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import calculateRoutes from "./src/routes/calculate.routes.js";
+import exportRoutes from "./src/routes/export.routes.js";
+
+const app = express();
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+app.use("/api/calculate", calculateRoutes);
+app.use("/api/export", exportRoutes);
+
+app.get("/", (_, res) => res.send("Sheet Planner Pro API Running"));
+
+app.listen(5000, () => console.log("Server started"));
